31f728d44df29ed4dfb86ef7d864b484e23ee975
weiyihu <weiyh@missfresh.cn>
2018-12-22 13:30:09 +0800

    fix

--- pages/active/active.js
+++ pages/active/active.js
@@ -1,2 +1,16 @@
 Page({
-});
+    data: {
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+    },
+    //获取当前滑块的index
+    toDetail (e) {
+        wx.navigateTo({
+            url: '/pages/active/detail'
+        })
+    }
+   
+  })
